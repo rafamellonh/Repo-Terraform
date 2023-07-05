05947d1ec66209c6f602559ec6d7d7186834c370
Rafael <rmellonh@gmail.com>
2023-07-04 22:19:36 -0300

    Terraform

--- Network/Peering.tf
+++ Network/Peering.tf
@@ -11,3 +11,17 @@ resource "azurerm_virtual_network_peering" "peer-vnet-hubTOvnet01" {
   virtual_network_name      = azurerm_virtual_network.vnet-hub.name
   remote_virtual_network_id = azurerm_virtual_network.vnet01.id
 }
+
+resource "azurerm_virtual_network_peering" "PEER-vnet-hubTOvnetdev" {
+  name                      = "PEER-vnet-hubTOvnetdev"
+  resource_group_name       = azurerm_resource_group.RG01.name
+  virtual_network_name      = azurerm_virtual_network.vnet-hub.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet-dev.id
+}
+
+resource "azurerm_virtual_network_peering" "PEER-vnet-devTOvnet-hub" {
+  name                      = "PEER-vnet-devTOvnet-hub"
+  resource_group_name       = azurerm_resource_group.RG01.name
+  virtual_network_name      = azurerm_virtual_network.vnet-dev.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet-hub.id
+}
---
+++ Network/bastion.tf
@@ -0,0 +1,19 @@
+resource "azurerm_public_ip" "pip-bst" {
+  name = "pip-bst"
+  resource_group_name = azurerm_resource_group.RG01.name
+  location = azurerm_resource_group.RG01.location
+  allocation_method = "Static"
+  sku = "Standard"
+}
+
+resource "azurerm_bastion_host" "bst01" {
+  name = "bst01"
+  resource_group_name = azurerm_resource_group.RG01.name
+  location = azurerm_resource_group.RG01.location
+
+  ip_configuration {
+    name = "confs"
+    subnet_id = azurerm_subnet.AzureBastionSubnet.id
+    public_ip_address_id = azurerm_public_ip.pip-bst.id
+  }
+}
